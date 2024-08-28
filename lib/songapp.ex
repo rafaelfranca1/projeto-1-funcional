1dd13b69db88139d5116bb3ddecc0ec9a474b31f
Lucas Gabriel Fontes da Silva <113773187+LucasGabrielFontes@users.noreply.github.com>
2024-08-28 19:04:44 -0300

    feature para buscar músicas por letra e artista

--- lib/songapp.ex
+++ lib/songapp.ex
@@ -1,42 +1,41 @@
 defmodule Songapp do
-  @api_url "https://genius.com/api-clients"
+  @api_url "https://api.genius.com/search"
   @api_key "KA4XOUPd0ERQumuJmB2lE1j24oRF_MOLVjBzB_2QSPibp4d0OEv1awCUsnJSuo0b"
   @header [{"Authorization", "Bearer #{@api_key}"}]
 
-  def teste() do
-    case  HTTPoison.get(@api_url, @header, follow_redirect: true) do
+  def search_song(query) do
+    encoded_query = URI.encode(query)
+    url = "#{@api_url}?q=#{encoded_query}"
+
+    case HTTPoison.get(url, @header, follow_redirect: true) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, parse_response(body)}
+        case Poison.decode(body) do
+          {:ok, decoded_body} ->
+            extract_song_info(decoded_body)
+          {:error, error} ->
+            IO.puts("Erro ao decodificar JSON: #{inspect(error)}")
+            {:error, {:invalid_json, error}}
+        end
 
-      {:ok, %HTTPoison.Response{status_code: 302, headers: headers}} ->
-        new_url = get_location(headers)
-        HTTPoison.get(new_url, headers)
+      {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
+        IO.puts("Erro HTTP: #{status_code}")
+        IO.puts("Corpo da Resposta: #{body}")
+        {:error, {:http_error, status_code, body}}
 
-      {:ok, %HTTPoison.Response{status_code: status_code}} ->
-        {:error, "Failed to fetch data. Status code: #{status_code}"}
-
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, "Request failed: #{reason}"}
+      {:error, error} ->
+        IO.puts("Falha na requisição: #{inspect(error)}")
+        {:error, {:request_failed, error}}
     end
   end
 
-  defp parse_response(body) do
-    body
-    |> Poison.decode!()
-    |> Enum.map(&song_to_map/1)
-  end
+  defp extract_song_info(%{"response" => %{"hits" => [hit | _]}}) do
+    result = hit["result"]
 
-  defp song_to_map(issue) do
     %{
-      title: issue["title"],
-      url: issue["html_url"],
-      state: issue["state"]
+      title: result["title"],
+      artist: result["primary_artist"]["name"],
+      release_date: result["release_date_for_display"],
+      song_url: result["url"],
     }
-  end
-
-  defp get_location(headers) do
-    headers
-    |> Enum.find(fn {key, _value} -> key == "Location" end)
-    |> elem(1)
   end
 end
