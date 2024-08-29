170c8034afd739a524827e201910a3b44cdaed7f
j4marcos <joaomarcos2004.jonh@gmail.com>
2024-08-29 01:12:37 -0300

    feat: CLI

--- mix.exs
+++ mix.exs
@@ -7,24 +7,27 @@ defmodule Songapp.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: escript_config()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :httpoison, :poison]
     ]
+  end
+
+  defp escript_config do
+    [main_module: CLI] # Modifique para o módulo principal correto
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:httpoison, "~> 2.1"},
-      {:poison, "~> 5.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:poison, "~> 5.0"}    
     ]
   end
 end
