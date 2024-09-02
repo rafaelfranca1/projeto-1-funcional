4d27965aadd3fc3db13b1a03f72008aaba61b739
rafaelfranca1 <rafaeldefranca750@gmail.com>
2024-09-02 19:36:09 -0300

    fix: mix.exs file

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Songapp.MixProject do
   defp deps do
     [
       {:httpoison, "~> 2.1"},
-      {:poison, "~> 6.0"}, 
+      {:poison, "~> 5.0"},
       {:floki, "~> 0.36.2"},
       {:jason, "~> 1.2"},
       {:tesla, "~> 1.12"}
